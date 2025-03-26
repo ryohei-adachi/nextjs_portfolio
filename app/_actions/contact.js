ffff6f24bafcc17488c6c7fe333d95d9bd41369f
logintech12345 <logintech12345@gmail.com>
2025-03-27 06:56:00 +0900

    お問い合わせ処理の作成2

--- app/_actions/contact.js
+++ app/_actions/contact.js
@@ -9,21 +9,21 @@ function validateEmail(email) {
 export async function createContactData(_prevState, formData) {
     
     //formのname属性からフォームデータを取得する
-    const rawData = {
+    const rawFormData = {
         name: formData.get("name"),
         email: formData.get("email"),
         message: formData.get("message")
     }
     
     //名前が未入力の場合、エラーを表示する
-    if (!rawData.name) {
+    if (!rawFormData.name) {
         return {
             status: "error",
             message: "名前を入力してください",
         };
     }
     //メールアドレスが未入力の場合、エラーを表示する
-    if (!rawData.email) {
+    if (!rawFormData.email) {
         return {
             status: "error",
             message: "メールアドレスを入力してください",
@@ -31,7 +31,7 @@ export async function createContactData(_prevState, formData) {
     }
 
     //メールアドレスの形式が誤っている場合、エラーを表示する
-    if (!validateEmail(rawData.email)) {
+    if (!validateEmail(rawFormData.email)) {
         return {
             status: "error",
             message: "メールアドレスの形式が誤っています",
@@ -39,11 +39,51 @@ export async function createContactData(_prevState, formData) {
     }
 
     //メッセージが未入力の場合、エラーを表示する
-    if (!rawData.message) {
+    if (!rawFormData.message) {
         return {
             status: "error",
             message: "メッセージを入力してください",
         };
+    }
+
+    const result = await fetch (
+        `https://api.hsforms.com/submissions/v3/integration/submit/${process.env.HUBSPOT_PORTAL_ID}/${process.env.HUBSPOT_FORM_ID}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                fields: [
+                    {
+                        objectTypeId: "0-1",
+                        name: "firstname",
+                        value: rawFormData.name,
+                    },
+                    {
+                        objectTypeId: "0-1",
+                        name: "email",
+                        value: rawFormData.email,
+                    },
+                    {
+                        objectTypeId: "0-1",
+                        name: "message",
+                        value: rawFormData.message,
+                    }
+                ],
+            })
+        }
+    );
+
+    try {
+        await result.json();
+    }
+    catch(e) {
+        console.log(e);
+        return {
+            status: "error",
+            message: "お問い合わせに失敗しました。"
+        }
     }
 
     return { status: "success", message: "OK"};
